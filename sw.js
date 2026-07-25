f68f2e339da7005cf2e2f55f3687636b88af1372
socraticstatic <socraticstatic@users.noreply.github.com>
2026-07-25 06:11:59 -0500

    Updates

--- sw.js
+++
@@ -1,9 +0,0 @@
-// Self-unregistering service worker.
-// Previous sw.js used SPA fallback (serving index.html for all navigations),
-// which broke standalone entry points like demo.html.
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', async () => {
-  await self.registration.unregister();
-  const keys = await caches.keys();
-  await Promise.all(keys.map(k => caches.delete(k)));
-});
